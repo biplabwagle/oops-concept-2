51ec04943461fbaf3247c136a9c637efe98ec3e1
biplab-w <waglebip@gmail.com>
2023-11-21 00:08:54 -0600

    Added state for Medicine java

--- src/oopsconceptsbasics/Medicine.java
+++ src/oopsconceptsbasics/Medicine.java
@@ -1,4 +1,21 @@
 package oopsconceptsbasics;
 
+import java.time.LocalDate;
+
 public class Medicine {
+    private String name;
+    private String brand;
+    private String dosageForm; //capsule, tablet, liquid or syrup forms etv
+    private String strength; // 5mg, 10 mg etc
+    private LocalDate manufactureDate;
+    private LocalDate expirationDate;
+    private String batchNumber; // for tracking and quality control
+    private String sideEffects; // desc of potential side effects
+    private String storageInstructions; // cold place, dark place?
+    private int quantity; //how many pills, milliliters if in liquid form
+
+    //Accessors and Mutators
+
+    //Behaviors/ Methods
+
 }
