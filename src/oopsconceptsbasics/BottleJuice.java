9e122a9202fd7d0349ad1b237382eac949d984e5
biplab-w <waglebip@gmail.com>
2023-11-20 23:45:06 -0600

    Initialized project and added BottleJuice class

---
+++ src/oopsconceptsbasics/BottleJuice.java
@@ -0,0 +1,17 @@
+package oopsconceptsbasics;
+
+public class BottleJuice {
+    //State
+    String name;
+    String ingredients;
+    String vitaminAndMinerals;
+
+    int servingsPerContainer;
+    int calories;
+    int sugarContent;
+    int totalCarbohydrates;
+    int dietaryFibre;
+    int sodium;
+    int fat;
+    //Behaviors
+}
