656b4aec65d2511e78cb8709ad994b5a6d11d45d
biplab-w <waglebip@gmail.com>
2023-11-23 09:13:00 -0600

    Added getters and setters for HeadPhone class

--- src/oopsconceptsbasics/HeadPhone.java
+++ src/oopsconceptsbasics/HeadPhone.java
@@ -17,5 +17,101 @@ public class HeadPhone {
 
     //Accessors and Mutators
 
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getConnectedDevice() {
+        return connectedDevice;
+    }
+
+    public void setConnectedDevice(String connectedDevice) {
+        this.connectedDevice = connectedDevice;
+    }
+
+    public String getBluetoothVersion() {
+        return bluetoothVersion;
+    }
+
+    public void setBluetoothVersion(String bluetoothVersion) {
+        this.bluetoothVersion = bluetoothVersion;
+    }
+
+    public boolean isWireless() {
+        return isWireless;
+    }
+
+    public void setWireless(boolean wireless) {
+        isWireless = wireless;
+    }
+
+    public boolean isHasActiveNoiseCancelling() {
+        return hasActiveNoiseCancelling;
+    }
+
+    public void setHasActiveNoiseCancelling(boolean hasActiveNoiseCancelling) {
+        this.hasActiveNoiseCancelling = hasActiveNoiseCancelling;
+    }
+
+    public int getBatteryLevel() {
+        return batteryLevel;
+    }
+
+    public void setBatteryLevel(int batteryLevel) {
+        this.batteryLevel = batteryLevel;
+    }
+
+    public int getVolumeLevel() {
+        return volumeLevel;
+    }
+
+    public void setVolumeLevel(int volumeLevel) {
+        this.volumeLevel = volumeLevel;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public boolean isHasTouchControls() {
+        return hasTouchControls;
+    }
+
+    public void setHasTouchControls(boolean hasTouchControls) {
+        this.hasTouchControls = hasTouchControls;
+    }
+
+    public int getListeningHours() {
+        return listeningHours;
+    }
+
+    public void setListeningHours(int listeningHours) {
+        this.listeningHours = listeningHours;
+    }
+
     //Behaviors
 }
