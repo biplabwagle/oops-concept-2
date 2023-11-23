1d343c88dfe4c2f3d4ee4aca5a259d303c2ea5c7
biplab-w <waglebip@gmail.com>
2023-11-23 09:10:45 -0600

    Added setters and getters

--- src/oopsconceptsbasics/BottleJuice.java
+++ src/oopsconceptsbasics/BottleJuice.java
@@ -21,6 +21,126 @@ public class BottleJuice {
     private LocalDate expiryDate;
     //Accessors and Mutators
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String[] getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String[] ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String[] getVitaminAndMinerals() {
+        return vitaminAndMinerals;
+    }
+
+    public void setVitaminAndMinerals(String[] vitaminAndMinerals) {
+        this.vitaminAndMinerals = vitaminAndMinerals;
+    }
+
+    public String getDisclaimers() {
+        return disclaimers;
+    }
+
+    public void setDisclaimers(String disclaimers) {
+        this.disclaimers = disclaimers;
+    }
+
+    public boolean isHasAntiOxidants() {
+        return hasAntiOxidants;
+    }
+
+    public void setHasAntiOxidants(boolean hasAntiOxidants) {
+        this.hasAntiOxidants = hasAntiOxidants;
+    }
+
+    public int getServingsPerContainer() {
+        return servingsPerContainer;
+    }
+
+    public void setServingsPerContainer(int servingsPerContainer) {
+        this.servingsPerContainer = servingsPerContainer;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public int getSugarContent() {
+        return sugarContent;
+    }
+
+    public void setSugarContent(int sugarContent) {
+        this.sugarContent = sugarContent;
+    }
+
+    public int getTotalCarbohydrates() {
+        return totalCarbohydrates;
+    }
+
+    public void setTotalCarbohydrates(int totalCarbohydrates) {
+        this.totalCarbohydrates = totalCarbohydrates;
+    }
+
+    public int getTotalProtein() {
+        return totalProtein;
+    }
+
+    public void setTotalProtein(int totalProtein) {
+        this.totalProtein = totalProtein;
+    }
+
+    public int getDietaryFibre() {
+        return dietaryFibre;
+    }
+
+    public void setDietaryFibre(int dietaryFibre) {
+        this.dietaryFibre = dietaryFibre;
+    }
+
+    public int getSodium() {
+        return sodium;
+    }
+
+    public void setSodium(int sodium) {
+        this.sodium = sodium;
+    }
+
+    public int getFat() {
+        return fat;
+    }
+
+    public void setFat(int fat) {
+        this.fat = fat;
+    }
+
+    public LocalDate getManufactureDate() {
+        return manufactureDate;
+    }
+
+    public void setManufactureDate(LocalDate manufactureDate) {
+        this.manufactureDate = manufactureDate;
+    }
+
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(LocalDate expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
     //Behaviors
 
 
