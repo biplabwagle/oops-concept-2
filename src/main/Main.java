6143e54eb59d46f023c43ca03c4bdb7ed48d1a3e
biplab-w <waglebip@gmail.com>
2023-11-23 09:11:23 -0600

    Added new BottleJuice Object

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,10 @@
+package main;
+
+import oopsconceptsbasics.BottleJuice;
 
 public class Main {
     public static void main(String[] args) {
+        BottleJuice orangeJuice = new BottleJuice();
 
-}}
+}
+}
